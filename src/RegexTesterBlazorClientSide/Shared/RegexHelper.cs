dc058437136b846a54d073bff6d15da20d220d49
Serhii Lutai <lsvhome@gmail.com>
2019-07-02 02:29:16 +0300

    3

---
+++ src/RegexTesterBlazorClientSide/Shared/RegexHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace RegexTesterBlazorClientSide.Shared
+{
+    public class RegexHelper
+    {
+        //public static string[] ff(string pattern, string text)
+        //{
+        //    var g = Regex.Matches(text, pattern);
+        //    foreach(Match e in g)
+        //    {
+        //        //e.Success;
+        //    }
+        //}
+    }
+}
