75c91c85e6ce566f38616f45737a04554630b155
Serhii Lutai <lsvhome@gmail.com>
2019-07-07 13:53:43 +0300

    fix2

--- publish.cmd
+++ publish.cmd
@@ -19,7 +19,7 @@ cd src
 dotnet clean
 
 dotnet  publish -c Release -o %dst%
-git --git-dir=%dst2%\.git --work-tree=%dst2% add . -u -f 
+git --git-dir=%dst2%\.git --work-tree=%dst2% add . -f
 git --git-dir=%dst2%\.git --work-tree=%dst2% status
 git --git-dir=%dst2%\.git --work-tree=%dst2% commit -m "Built %master-sha%"
 git --git-dir=%dst2%\.git --work-tree=%dst2% push origin gh-pages:gh-pages
