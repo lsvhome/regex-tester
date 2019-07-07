c6d2a653d7ab79d7d270f00c6c04325702799fa6
Serhii Lutai <lsvhome@gmail.com>
2019-07-07 12:02:14 +0300

    10

--- src/RegexTesterBlazorClientSide/Pages/IndexController.cs
+++ src/RegexTesterBlazorClientSide/Pages/IndexController.cs
@@ -11,6 +11,7 @@ namespace RegexTesterBlazorClientSide.Shared
 {
     public class IndexController
     {
+        /*
         public static string[] ff(string pattern, string text)
         {
             System.Text.RegularExpressions.Regex regex = new System.Text.RegularExpressions.Regex(@"abcd.*");
@@ -19,7 +20,7 @@ namespace RegexTesterBlazorClientSide.Shared
             RegexOptions s = RegexOptions.None;
             //Enum.Parse(typeof(RegexOptions),h)
             //bool c = s & Enum.GetValues(typeof(RegexOptions))
-            Dictionary<string, string> RegexOpt  = new Dictionary<string, string>();
+            Dictionary<string, string> RegexOpt = new Dictionary<string, string>();
             //RegexOpt.ContainsKey()
             //Enum.GetValues()
             //string optionName = "";
@@ -38,22 +39,19 @@ namespace RegexTesterBlazorClientSide.Shared
 
             return null;
         }
-        public static MarkupString Decor(string text,string pattern, MatchCollection matches)
+        */
+        public static MarkupString GetDecoratedTextWithMatches(string input, string pattern, MatchCollection matches)
         {
-            Debug.WriteLine($"Decor 0");
-            //text = text.Replace("\r\n","\n") + "\n";
-            text = text + "\n";
-            gg[] g = new gg[text.Length];
+            input = input + "\n";
+            CharDecorator[] decoratedInput = new CharDecorator[input.Length];
             int row = 0;
-            for(int i = 0; i< text.Length; i++)
+            for (int i = 0; i < input.Length; i++)
             {
-                g[i] = new gg { x = text[i].ToString(), decorated = text[i].ToString() };
+                decoratedInput[i] = new CharDecorator { Symbol = input[i].ToString(), DecoratedSymbol = input[i].ToString(), Line = row };
 
-                if (g[i].x == "\n")
+                if (decoratedInput[i].Symbol == "\n")
                 {
-                    g[i].decorated = "<br />";
-                    g[i].row = row;
-                    Debug.WriteLine($"Decor 1");
+                    decoratedInput[i].DecoratedSymbol = "<br />";
                     row++;
                 }
             }
@@ -64,53 +62,48 @@ namespace RegexTesterBlazorClientSide.Shared
             {
                 string[] names = new Regex(pattern).GetGroupNames();
 
-                foreach (Match m in matches)
+                foreach (Match match in matches)
                 {
-                    //foreach (string s in names)
                     for (int n = 0; n < names.Length; n++)
                     {
-                        string s = names[n];
-                        var gr = m.Groups[s];
-                        for (int i = gr.Index; i < gr.Index + gr.Length; i++)
+                        string groupName = names[n];
+                        var group = match.Groups[groupName];
+                        for (int charIndex = group.Index; charIndex < group.Index + group.Length; charIndex++)
                         {
-                            //var b1 = m.Groups["xx"];
-                            //bool b = gr[].GetType().GetProperty("Name") != null;
-                            //var b = gr.ToString();
-                            g[i].decorated = $"<span class=\"group_{n}\">{g[i].x}</span>";
+                            decoratedInput[charIndex].DecoratedSymbol = $"<span class=\"group_{n}\">{decoratedInput[charIndex].Symbol}</span>";
                         }
-
                     }
                 }
 
-                for (int i = 0; i < g.Length; i++)
+                for (int i = 0; i < decoratedInput.Length; i++)
                 {
-                    ret0.Append(string.IsNullOrWhiteSpace(g[i].decorated) ? g[i].x : g[i].decorated);
+                    ret0.Append(string.IsNullOrWhiteSpace(decoratedInput[i].DecoratedSymbol) ? decoratedInput[i].Symbol : decoratedInput[i].DecoratedSymbol);
                 }
 
-                //ret0 = ret0.Replace("<br />< br />", "<br />");
-
                 int last = 0;
-                for (int i = 0; i < text.Length; i++)
+                for (int i = 0; i < input.Length; i++)
                 {
-                    if (g[i].x == "\n")
+                    if (decoratedInput[i].Symbol == "\n")
                     {
                         var q = from f in matches.Cast<Match>()
-                                where f.Index >= last && f.Index+f.Length<=i
+                                where f.Index >= last && f.Index + f.Length <= i
                                 select f;
                         Debug.WriteLine($"{i} q.cnt={q.Count()}");
 
 
-                        var qt = q.Select(each => {
+                        var qt = q.Select(each =>
+                        {
                             var t = names.Select(x => $"{{{x}}}='{each.Groups[x].Value}'");
                             string s = $"[ {each.Value} : {string.Join(" | ", t)} ]";
 
-                            return s; });
+                            return s;
+                        });
 
                         Debug.WriteLine($"{i} qt.cnt={qt.Count()}");
 
-                        g[i].decorated = $"&nbsp;&nbsp;&nbsp;{string.Join("&nbsp;&nbsp;&nbsp;", qt)}<br />";
+                        decoratedInput[i].DecoratedSymbol = $"&nbsp;&nbsp;&nbsp;{string.Join("&nbsp;&nbsp;&nbsp;", qt)}<br />";
                         last = i;
-                        ret1.Append(string.IsNullOrWhiteSpace(g[i].decorated) ? g[i].x : g[i].decorated);
+                        ret1.Append(string.IsNullOrWhiteSpace(decoratedInput[i].DecoratedSymbol) ? decoratedInput[i].Symbol : decoratedInput[i].DecoratedSymbol);
                     }
                 }
 
@@ -127,11 +120,11 @@ namespace RegexTesterBlazorClientSide.Shared
             return (MarkupString)ff;// new MarkupString[2] { (MarkupString)ret0.ToString(), (MarkupString)ret1.ToString() };
         }
 
-        class gg
+        public class CharDecorator
         {
-            public int row = 0;
-            public string x;
-            public string decorated;
+            public int Line { get; set; } = 0;
+            public string Symbol { get; set; }
+            public string DecoratedSymbol { get; set; }
         }
     }
 }
