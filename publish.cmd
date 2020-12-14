980891ae5b5a616cfe1c3903b44dba13b98de666
Sergii Lutai sergii.lutai.ext@dieboldnixdorf.com <sergii.lutai.ext@dieboldnixdorf.com>
2020-12-14 17:17:25 +0200

    upgrade publish.cmd

--- publish.cmd
+++ publish.cmd
@@ -10,23 +10,38 @@ SET master-sha=%%F
 
 set dst=%dstx%\publish
 set dsttmp=%dstx%\temp
-set dst2=%dst%\RegexTesterBlazorClientSide\dist
+set dst2=%dst%\wwwroot
 md %dst2%
 md %dsttmp%
 git branch gh-pages origin/gh-pages
 git clone --separate-git-dir=%dst2%\.git  -l -b gh-pages . %dsttmp% 
 
 cd src
+
 dotnet clean
+
+cd RegexTesterBlazorClientSide
 
 dotnet  publish -c Release -o %dst%
 
-git --git-dir=%dst2%\.git --work-tree=%dst2% add . -f
-git --git-dir=%dst2%\.git --work-tree=%dst2% status
-git --git-dir=%dst2%\.git --work-tree=%dst2% commit -m "Built %master-sha%" --allow-empty
-git --git-dir=%dst2%\.git --work-tree=%dst2% push origin gh-pages:gh-pages
+cd %dst2%
 
-rd /s /q %dstx%
+del *.br /S
+del *.gz /S
+del *.log /S
 
+git ls-files --deleted -z | xargs -0 git rm
+git add -u -f :/
+git add -f :/
+git commit -m "Built %master-sha%" --allow-empty
+git push origin gh-pages:gh-pages
+
+cd ..
+mklink /D /J regex-tester wwwroot
+start http-server
+start chrome -incognito http://localhost:8080/regex-tester
+cd %srcDir%
 gitk HEAD gh-pages
+pause
+rem rd /s /q %dstx%
 
